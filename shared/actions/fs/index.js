d7ad01f3eb3d387c0cbf69895371c4883677e0dd
Jakob Weisblat <jakob@keyba.se>
2019-05-13 18:15:01 -0400

    fs: CR: implement "begin manual resolution" link
    Issue: KBFS-4151

--- shared/actions/fs/index.js
+++ shared/actions/fs/index.js
@@ -911,6 +911,16 @@ const waitForKbfsDaemon = (state, action) => {
     })
 }
 
+const startManualCR = (state, action) =>
+  RPCTypes.SimpleFSSimpleFSClearConflictStateRpcPromise({
+    path: Constants.pathToRPCPath(action.payload.tlfPath),
+  }).then(() =>
+    FsGen.createTlfCrStatusChanged({
+      status: 'in-manual-resolution',
+      tlfPath: action.payload.tlfPath,
+    })
+  ) // TODO: deal with errors
+
 const updateKbfsDaemonOnlineStatus = (state, action) =>
   state.fs.kbfsDaemonStatus.rpcStatus === 'connected' && state.config.osNetworkOnline
     ? RPCTypes.SimpleFSSimpleFSAreWeConnectedToMDServerRpcPromise().then(connectedToMDServer =>
@@ -1009,6 +1019,12 @@ function* fsSaga(): Saga.SagaGenerator<any, any> {
     yield* Saga.chainAction<EngineGen.Keybase1NotifyFSFSOverallSyncStatusChangedPayload>(
       EngineGen.keybase1NotifyFSFSOverallSyncStatusChanged,
       onFSOverallSyncSyncStatusChanged
+    )
+  }
+  if (flags.conflictResolutionGui) {
+    yield* Saga.chainAction<FsGen.StartManualConflictResolutionPayload>(
+      FsGen.startManualConflictResolution,
+      startManualCR
     )
   }
 
